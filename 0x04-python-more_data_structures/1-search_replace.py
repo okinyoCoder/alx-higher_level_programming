2124b72d1047d3e1dcec66721114adda3687d4e0
okinyoCoder <okinyoanthony@gmail.com>
2022-11-24 08:22:15 +0000

    function that replaces all occurrences of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for x in my_list:
+        if x == search:
+            my_list[my_list.index(search)] = replace
+            new_list.append(x)
+    return new_list
