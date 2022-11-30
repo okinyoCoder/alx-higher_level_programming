897f7486db9ab20a397a18f3250514e582b14b96
okinyoCoder <okinyoanthony@gmail.com>
2022-11-30 07:36:06 +0000

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const x = parseint(process.argv[2]);
+const ch = 'X';
+
+if (isNaN(x){
+	console.log('Missing size');
+}else{
+	for ( let i = 0; i < x; i++){
+		for ( let j = 0; j < i; j++){
+			console.log(ch);
+		}
+	}
+}
