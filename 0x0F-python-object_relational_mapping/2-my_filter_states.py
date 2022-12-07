9369f291cd6a0ea3221892663f154dd4f30afefc
okinyoCoder <okinyoanthony@gmail.com>
2022-12-07 11:27:34 +0000

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,18 @@
+
 #!/usr/bin/python3
-"""
-script that list all states with name starting with N
-from a database hbtn_0e_0_usa 
-"""
-import MySQLdb
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
 import sys
-
-def search_state(username, password, databaseName):
-    dbase = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user="username",
-            passwd="password",
-            db="databaseName"
-            )
-    cur = dbase.cursor()
-    cur.execute(
-            """SELECT * FROM states WHERE name 
-            LIKe 'N%' ORDER BY state.id ASC"""
-            )
-    result = cur.fetchall()
-
-    for row in result:
-        print(row)
-
-    cur.close()
-    dbase.close()
+import MySQLdb
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    username, password, db_nam = argv
-    search_state(username, password, db_nam)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
