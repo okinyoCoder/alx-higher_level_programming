e5746fef53501aa3b52cc7365eaf1ce1f4721df0
okinyoCoder <okinyoanthony@gmail.com>
2022-12-22 11:34:03 +0000

    sends a JSON POST request to a URL passed as the first argument, and displays the body of the response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -3,4 +3,4 @@
 # displays the body of the response. 
 # Also send a POST request with the contents of a file, passed with the filename as 
 # the second argument of the script, in the body of the request.
-curl -s "$1" -X POST -H "Content-Type: application/json" -d "$(cat "$2")"
+curl -sX POST -d "@$2" -H "Content-Type: application/json" "$1"
