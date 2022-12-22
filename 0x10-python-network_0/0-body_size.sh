8beb3ee2fb0be65c062446703e188779ab3c5072
okinyoCoder <okinyoanthony@gmail.com>
2022-12-22 09:54:30 +0000

    script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
 # takes in a URL, sends a request to that URL, and 
 # displays the size of the body of the response
 
-curl -sI "$1" | grep "Content-Length:" |  cut -d ' '  -f 2
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f 2
