b97088052aaf543ed3144f7b3cfd342838fa8578
okinyoCoder <okinyoanthony@gmail.com>
2023-01-23 14:50:12 +0000

    correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivionError:
-           result = None
+    except ZeroDivisionError:
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return result
