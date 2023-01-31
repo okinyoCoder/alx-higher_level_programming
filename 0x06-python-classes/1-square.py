6feaf890b60575549bd2685c97ac2b1120d9e631
okinyoCoder <okinyoanthony@gmail.com>
2023-01-31 14:04:28 +0000

    class Square that defines a square correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-'''Square class with a private instance
-   attritube
-'''
+'''Square class with a private instance attritube'''
 
-class Square:
+class Square():
+    '''class constructor'''
     def __init__(self, size):
-        '''class constructor'''
         self.__size = size
