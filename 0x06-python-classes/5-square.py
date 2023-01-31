b3fd91ce944659b53c91ad7b4338431b3dddab07
okinyoCoder <okinyoanthony@gmail.com>
2023-01-31 16:58:43 +0000

    Access and update private attribute correction

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" A class Square that defines a square by:
+    Private instance attribute: size
+    Public instance method: def area(self)
+"""
+
+
+class Square():
+    """class constructor"""
+    def __init__(self, size = 0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Returns  area of a square"""
+        return self.__size ** 2
+
+    """size getter"""
+    @property
+    def size(self):
+        return self.__size
+
+    """size setter"""
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
