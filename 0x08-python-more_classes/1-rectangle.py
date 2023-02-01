e4be672ec72de78175e1fc68b2fe69938004f364
okinyoCoder <okinyoanthony@gmail.com>
2023-02-01 16:43:38 +0000

    Real definition of a rectangle second corretion

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
             TypeError: If `value` type is not `int`.
             ValueError: If `value` is less than `0`.
         """
-        if type(value) !=  int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -63,7 +63,7 @@ class Rectangle:
             TypeError: If `value` type is not `int`.
             ValueError: If `value` is less than `0`.
         """
-        if type(value) !=  int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
