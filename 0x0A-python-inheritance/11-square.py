29b7945a8dee59ef42c12b9b3030e56c730b2746
okinyoCoder <okinyoanthony@gmail.com>
2023-02-08 19:16:07 +0000

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,18 +1,13 @@
-"""Square #2"""
-
+#!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ inherits from Rectangle """
-
     def __init__(self, size):
-        """ Constructor """
-        self.integer_validator("size", size)
-        self.__size = size
+        self.integer_validator(size, size)
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
-        """ print(self) method """
-        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
