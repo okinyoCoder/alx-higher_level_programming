5f8c2de7dee8c3a37c4b0700111d363899864b89
okinyoCoder <okinyoanthony@gmail.com>
2023-03-16 04:57:08 +0000

    script that lists all the cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Script that lists all the cities of California registered in the database
+-- Query to list all the cities from California
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California');
