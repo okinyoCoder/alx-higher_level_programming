3da60529b741f496321c07ab2e4b332241bc42a7
okinyoCoder <okinyoanthony@gmail.com>
2023-04-11 22:57:00 +0000

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,12 +5,13 @@ The first argument is the size of the square
 If the first argument can’t be converted to an integer,
   print “Missing size”
 */
-const x = parseInt(process.argv[2]);
+const j = process.argv;
 const ch = 'X';
 
-if (isNaN(x)) {
+if (isNaN(j)) {
   console.log('Missing size');
 } else {
+  const x = parseInt(j);
   for (let i = 1; i < x; i++) {
     for (let j = 1; j < i; j++) {
       console.log(ch);
