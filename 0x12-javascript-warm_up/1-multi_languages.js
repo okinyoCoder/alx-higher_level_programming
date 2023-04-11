3a49e465549f32cfabc813ec85c95bc6363488a9
okinyoCoder <okinyoanthony@gmail.com>
2023-04-11 18:07:27 +0000

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@
 /*
 script that prints 3 lines
 */
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+
+const word = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(word);
