dac4d00ec2f631ba36e402a0f73c93e7dd0a4b9c
okinyoCoder <okinyoanthony@gmail.com>
2023-04-11 21:52:11 +0000

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,16 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+/*
+Write a script that prints My number:
+ <first argument converted in integer>
+if the first argument can be converted to an integer:
+if the argument can’t be converted to an integer,
+ print “Not a number”
+*/
+
+const process = require('process');
+const arrayVar = process.argv;
+if (isNaN(arrayVar[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + arrayVar[2]);
+}
