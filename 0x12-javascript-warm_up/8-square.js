8db2d035bfe810130a0e8908783ca0af1b46d136
okinyoCoder <okinyoanthony@gmail.com>
2023-04-11 22:37:05 +0000

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
-const x = parseint(process.argv[2]);
+/*
+A script that prints a square
+The first argument is the size of the square
+If the first argument can’t be converted to an integer,
+  print “Missing size”
+*/
+const x = parseInt(process.argv[2]);
 const ch = 'X';
 
-if (isNaN(x){
-	console.log('Missing size');
-}else{
-	for ( let i = 1; i < x; i++){
-		for ( let j = 1; j < i; j++){
-			console.log(ch);
-		}
-	}
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 1; i < x; i++) {
+    for (let j = 1; j < i; j++) {
+      console.log(ch);
+    }
+  }
 }
