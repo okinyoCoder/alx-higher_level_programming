451260aa775f8a93f0ffc6bfb5eef58da43f85b7
okinyoCoder <okinyoanthony@gmail.com>
2023-04-11 18:47:19 +0000

    Arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+/*
+script that prints a message depending
+of the number of arguments passed
+*/
+
+const argvLength = process.argv.length;
+if (argvLength < 3) {
+  console.log('No argument');
+} else if (argvLength === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
