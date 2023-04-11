b8836f80eef3945f625920d63e2e4cd9acf318f0
okinyoCoder <okinyoanthony@gmail.com>
2023-04-11 22:24:34 +0000

    script that prints x times “C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/*
+script that prints x times “C is fun”
+If the first argument can’t be converted to an integer,
+ it prints “Missing number of occurrences”
+*/
 const x = Number(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
