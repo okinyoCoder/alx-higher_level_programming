92e4d96728f312084ba4579e1c6f48ba001734c0
okinyoCoder <okinyoanthony@gmail.com>
2023-04-12 17:13:36 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -19,15 +19,12 @@ class Rectangle {
   }
 
   rotate () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.height));
-    }
+    [this.height, this.width] = [this.width, this.height];
   }
 
   double () {
-    for (let i = 0; i < (this.height * 2); i++) {
-      console.log('X'.repeat(this.width * 2));
-    }
+    this.height *= 2;
+    this.width *= 2;
   }
 }
 module.exports = Rectangle;
