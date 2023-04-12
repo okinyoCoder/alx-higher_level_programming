20132cbf8398b3e0617bbe3ddb0f71bbe205f4cd
okinyoCoder <okinyoanthony@gmail.com>
2023-04-12 16:23:40 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -14,9 +14,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        console.log('X');
-      }
+      console.log('X'.repeat(this.width));
     }
   }
 }
