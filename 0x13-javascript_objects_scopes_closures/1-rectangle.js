540066a3ced20297e6c30a014783dedecf2c7459
okinyoCoder <okinyoanthony@gmail.com>
2023-04-12 14:09:14 +0000

    update Rectangle class with a constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ *class Rectangle that defines a rectangle
+ *constructor must take 2 arguments w and h
+*/
+
+class Rectangle {
+  // constructor method
+  constructor (w, h) {
+    this.height = h;
+    this.width = w;
+  }
+}
+module.exports = Rectangle;
