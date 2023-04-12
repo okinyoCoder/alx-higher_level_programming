7f2192bad63faab007743482d540a3d1842d2fd3
okinyoCoder <okinyoanthony@gmail.com>
2023-04-12 13:47:03 +0000

    updated rectangle class

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 /*
-script empty class Rectangle that defines a rectangle
+ *script empty class Rectangle that defines a rectangle
 */
 class Rectangle {}
+module.exports = Rectangle;
