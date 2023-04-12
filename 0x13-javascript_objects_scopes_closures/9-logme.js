815e1aa117259c8ad1cd87e8af073247f6b7137a
okinyoCoder <okinyoanthony@gmail.com>
2023-04-12 22:55:26 +0000

    Log me

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ *function that prints the number of arguments
+ * already printed and the new argument value
+*/
+let counter = 0;
+exports.logMe = function count (item) {
+  console.log(`${counter}: ${item}`);
+  counter += 1;
+};
