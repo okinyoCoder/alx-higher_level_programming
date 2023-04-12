1594d1e99ca010399203783f317d2a2c188c40f6
okinyoCoder <okinyoanthony@gmail.com>
2023-04-12 21:29:03 +0000

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ const prevClass = require('./5-square');
 class Square extends prevClass {
   charPrint (c) {
     if (typeof c === 'undefined') {
-      super.print();
+      this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         console.log('c'.repeat(this.width));
