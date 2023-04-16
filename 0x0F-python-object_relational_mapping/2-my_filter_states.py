fa3b1645d3ea14b3ead663c0ba257a23cf2b23ec
okinyoCoder <okinyoanthony@gmail.com>
2023-04-16 07:22:41 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,19 +6,18 @@
 import MySQLdb
 from sys import argv
 
-dbase = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-
-cur = dbase.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+if __name__ == '__main__':
+    dbase = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+    cur = dbase.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
         ORDER BY states.id ASC".format(argv[4]))
-rows = cur.fetchall()
-
-for row in rows:
-    print(row)
-cur.close()
-dbase.close()
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    dbase.close()
