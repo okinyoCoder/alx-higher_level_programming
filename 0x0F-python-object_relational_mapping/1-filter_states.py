6ffd35d8eb5efe6925c77de7f043427a3c7474cd
okinyoCoder <okinyoanthony@gmail.com>
2023-04-16 05:45:41 +0000

    correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    dbase = MYSQLdb.connect(
+    dbase = MySQLdb.connect(
             host="localhost",
             port="3306",
             user="argv[1]",
