05207527d62c7bd0121c6a246fd12330c3d2381d
okinyoCoder <okinyoanthony@gmail.com>
2023-04-21 06:52:32 +0000

    correction

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,8 +9,9 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            argv[1], argv[2], argv[3])
-    engine = create_engine(db_url, pool_pre_ping=True)
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
     session = Session()
 
