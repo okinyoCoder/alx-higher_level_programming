cc44105464ec9e084a23107ff3a0b83e41e89d26
okinyoCoder <okinyoanthony@gmail.com>
2023-04-21 19:33:05 +0000

    correction

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,16 +5,8 @@ from model_state import Base, State
 
 
 class City(Base):
-    """class represent table City of a database
-    Args:
-        Base():
-    Attribute:
-         __tablename__(str): name of the class 
-         id(int): primary_key of cities table
-         name(str): names of cities
-         state_id(int): foreign_key that link to state id
-    """
+    """class represent table City of a database"""
     __tablename__ = 'cities'
-    id = Column(Integer, PrimaryKey=True,nullable=False )
+    id = Column(Integer, PrimaryKey=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, nullable=False, ForeignKey='states.id')
