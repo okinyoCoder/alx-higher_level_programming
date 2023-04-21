f93a5162a90aff826e2d96c2f2a179c40b225abf
okinyoCoder <okinyoanthony@gmail.com>
2023-04-21 06:44:12 +0000

    correction

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-""" script that prints the State object
-    with the name passed as argument from the
-    database hbtn_0e_6_usa
 """
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
+
 from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-                argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    obj = session.query(State).filter(State.name == argv[4]).first()
+
+    state = session.query(State).filter(State.name == argv[4]).first()
     if state is not None:
         print('{0}'.format(state.id))
     else:
