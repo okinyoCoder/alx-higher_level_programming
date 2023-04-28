d2111c4dea86fc71b344ac42d42820370a637342
okinyoCoder <okinyoanthony@gmail.com>
2023-04-28 15:23:19 +0000

    Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and 
-# displays the body of the response. 
-# Also send a POST request with the contents of a file, passed with the filename as 
-# the second argument of the script, in the body of the request.
-curl -sX POST -d "@$2" -H "Content-Type: application/json" "$1"
+# sends a JSON POST request to a URL passed as the first argument
+curl -s "$1" -X POST -d @"$2" -H "Content-Type: application/json"
