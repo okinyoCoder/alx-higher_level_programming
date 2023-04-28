fbfaf61db5860cb0443699b5e85b44397dc78f7b
okinyoCoder <okinyoanthony@gmail.com>
2023-04-28 12:39:40 +0000

    updated the  0-body_size.sh file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and 
+# takes in a URL, sends a request to that URL, and
 # displays the size of the body of the response
-
 curl -sI "$1": | grep -iF 'Content-Length' | cut -d ' '  -f 2
