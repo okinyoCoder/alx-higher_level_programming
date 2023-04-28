e2745ff4c9af91dacf91001ec629445bdc38ecef
okinyoCoder <okinyoanthony@gmail.com>
2023-04-28 14:11:45 +0000

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Find a peak"""
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list
+       of unsorted integers
+    """
+    size_list = len(list_of_integers)
+    if size_list == 1:
+        return list_of_integers[0]
+    elif size_list == 2:
+        return max(list_of_integers)
+
+    mid = int(size_list / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
