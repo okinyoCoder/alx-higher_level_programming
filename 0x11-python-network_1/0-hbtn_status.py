975d0061c9e244556ac7aacfad09a57623825417
okinyoCoder <okinyoanthony@gmail.com>
2023-04-29 17:30:35 +0000

    updated the 0-hbtn_status.py file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+with urllib.request.urlopen(
+        https://alx-intranet.hbtn.io/status) as response:
+    html = response.read()
