1bc6ece5331a3a42ca6ef9e7c4ade8d3397a5921
okinyoCoder <okinyoanthony@gmail.com>
2023-04-29 17:41:52 +0000

    debugged

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-with urllib.request.urlopen(
-        'https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
