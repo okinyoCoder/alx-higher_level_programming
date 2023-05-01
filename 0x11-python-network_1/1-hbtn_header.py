96f1f0240fa166bd8f385b155ca7ad4e0ba4f208
okinyoCoder <okinyoanthony@gmail.com>
2023-05-01 07:04:28 +0000

    debugged 1-hbtn_header.py file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,5 +10,4 @@ import urllib.request
 if '__name__' == '__main__':
     req = urllib.request.Request(argv[1])
     with urllib.request.urlopen(req) as response:
-        page = response.read()
-        print(page.headers['X-Request-Id'])
+        print(response.headers['X-Request-Id'])
