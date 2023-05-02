60c5cd534d7aa8c3f415c17a2141f8c796599cf9
okinyoCoder <okinyoanthony@gmail.com>
2023-05-02 17:11:40 +0000

    updated 8-json_api.py file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
-    if len(argv)  => 2:
+    if len(argv) >= 2:
         value = argv[1]
     else:
         value = ''
