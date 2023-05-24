84325ac34f7a34455a0f1c71967558e3829f02a7
okinyoCoder <okinyoanthony@gmail.com>
2023-05-24 11:52:12 +0000

    debugged files

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const text = process.argv[3];
 
-fs.writeFile(filename, content, 'utf-8', (error) => {
+fs.writeFile(filePath, text, 'utf-8', (error) => {
   if (error) {
     console.log(error);
   }
