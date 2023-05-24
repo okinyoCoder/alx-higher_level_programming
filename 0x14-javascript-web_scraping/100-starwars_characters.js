66df3fcda81168229ac2b8bbea6b1a9010e4afb6
okinyoCoder <okinyoanthony@gmail.com>
2023-05-24 23:29:24 +0000

    script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const movies = JSON.parse(body);
+    for (character of movies.characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const person = JSON.parse(body);
+          console.log(person.name);
+        }
+      });
+    }
+  }
+});
