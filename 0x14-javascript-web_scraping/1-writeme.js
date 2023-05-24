faf454324d0e8ede13144427460cbba806ebd743
okinyoCoder <okinyoanthony@gmail.com>
2023-05-24 11:41:49 +0000

    correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const text = process.argv[3];
 
-fs.writeFile(filePath, text, 'utf-8', err => {
+fs.writeFile(filename, content, 'utf-8', (error) => {
   if (error) {
-    console.error(err);
+    console.log(error);
   }
 });
